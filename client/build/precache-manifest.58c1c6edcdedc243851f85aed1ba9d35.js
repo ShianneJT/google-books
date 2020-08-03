bf066bc391211b016462d50760c6d94c7c51bc67
Shianne <ShianneJT@gmail.com>
2020-08-03 14:48:58 -0600

    build

---
+++ client/build/precache-manifest.58c1c6edcdedc243851f85aed1ba9d35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea91f2caf676d8775af0b568a935a9e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a862c53b6c8fe76be64",
+    "url": "/static/css/main.146796c5.chunk.css"
+  },
+  {
+    "revision": "b8ff1d5047097206d814",
+    "url": "/static/js/2.86a8f797.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.86a8f797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a862c53b6c8fe76be64",
+    "url": "/static/js/main.060b49ee.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
